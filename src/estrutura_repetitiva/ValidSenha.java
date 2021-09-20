f55551fc655385328d27f5a8d8153723263a71a3
Viniciusmgm <vinicius.mirandagm@gmail.com>
2021-09-20 13:49:04 -0300

    inicio estruturas repetitivas

---
+++ src/estrutura_repetitiva/ValidSenha.java
@@ -0,0 +1,21 @@
+package estrutura_repetitiva;
+
+import java.util.Scanner;
+
+public class ValidSenha {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int senha = sc.nextInt();
+		
+		
+		while(senha != 2002) {
+			System.out.println("Senha Inválida");
+			senha = sc.nextInt();
+		}
+		System.out.println("Acesso Permitido");
+		sc.close();
+
+	}
+
+}
