4fca243cc0f622d350c6ba35785f2b9b4adb7b9e
Viniciusmgm <vinicius.mirandagm@gmail.com>
2021-09-29 10:16:27 -0300

    fim vetores

--- src/vetores/alocacao_quartos/application/Alocacao.java
+++ src/vetores/alocacao_quartos/application/Alocacao.java
@@ -1,5 +1,39 @@
 package vetores.alocacao_quartos.application;
 
-public class Alocacao {
+import java.util.Locale;
+import java.util.Scanner;
+import vetores.alocacao_quartos.entities.DadosCliente;
 
+public class Alocacao {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.printf("Quantos quartos serão alugados? ");
+		int n = sc.nextInt();
+		DadosCliente[] alocacao = new DadosCliente[10];
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println("\nAlocação #" + i);
+			
+			System.out.print("Nome: ");
+			String nome = sc.next();
+			
+			System.out.print("Email: ");
+			String email = sc.next();
+			
+			System.out.print("Quarto: ");
+			int quarto = sc.nextInt();
+			
+			alocacao[quarto] = new DadosCliente(nome, email, quarto);
+		}
+		
+		System.out.println("\nQuartos alugados:");
+		for(int i = 0; i < alocacao.length; i++) {
+			if(alocacao[i] != null) {
+				System.out.println(alocacao[i].toString(i));
+			}
+		}
+		sc.close();
+	}
 }
--- src/vetores/alocacao_quartos/entities/DadosCliente.java
+++ src/vetores/alocacao_quartos/entities/DadosCliente.java
@@ -1,5 +1,41 @@
 package vetores.alocacao_quartos.entities;
 
 public class DadosCliente {
+	private String nome;
+	private String email;
+	private int quarto;
+	
+	public DadosCliente(String nome, String email, int quarto) {
+		this.nome = nome;
+		this.email = email;
+		this.quarto = quarto;
+	}
 
+	public int getQuarto() {
+		return quarto;
+	}
+
+	public void setQuarto(int quarto) {
+		this.quarto = quarto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String toString(int i) {
+		return String.format("Quarto %d: %s, %s", i, nome, email);
+	}
 }
