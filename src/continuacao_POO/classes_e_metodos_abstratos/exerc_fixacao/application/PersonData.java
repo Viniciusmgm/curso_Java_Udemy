aa8b7a759fd2babaa1ea3dc8840c3ca11f59eadd
Viniciusmgm <vinicius.mirandagm@gmail.com>
2021-11-10 10:14:30 -0300

    fim exerc fixação metodos abstratos

--- src/continuacao_POO/classes_e_metodos_abstratos/exerc_fixacao/application/PersonData.java
+++ src/continuacao_POO/classes_e_metodos_abstratos/exerc_fixacao/application/PersonData.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import continuacao_POO.classes_e_metodos_abstratos.exerc_fixacao.entities.LegalPerson;
 import continuacao_POO.classes_e_metodos_abstratos.exerc_fixacao.entities.Person;
+import continuacao_POO.classes_e_metodos_abstratos.exerc_fixacao.entities.PhysicalPerson;
 
 public class PersonData {
 	public static void main(String[] args) {
@@ -14,10 +16,41 @@ public class PersonData {
 		
 		List<Person> listPerson = new ArrayList<>();
 		
-		System.out.println("Enter number of tax payers: ");
+		System.out.printf("Enter number of tax payers: ");
 		int n = sc.nextInt();
 		for(int i = 0; i < n; i++) {
-			System.out.printf("Tax payer #%d ", i + 1);
+			System.out.printf("Tax payer #%d \n", i + 1);
+			
+			Person person;
+			
+			System.out.printf("Individual or company (i/c)? ");
+			char typePerson = sc.next().charAt(0);
+			
+			System.out.printf("Name: ");
+			String name = sc.next();
+			sc.nextLine();
+			
+			System.out.printf("Annual Income: ");
+			double annualIncome = sc.nextDouble();
+			
+			if(typePerson == 'i') {
+				System.out.printf("Health expenditures: ");
+				double healthExpenses = sc.nextDouble();
+				
+				person = new PhysicalPerson(name, annualIncome, healthExpenses);
+			}
+			else {
+				System.out.printf("Number of employees: ");
+				int numEmployee = sc.nextInt();
+				
+				person = new LegalPerson(name, annualIncome, numEmployee);
+			}
+			listPerson.add(person);
+		}
+		
+		System.out.println("\nTAXES PAID:");
+		for(Person person : listPerson) {
+			System.out.printf("%s: $ %.2f \n", person.getName(), person.tax());
 		}
 		
 		sc.close();
