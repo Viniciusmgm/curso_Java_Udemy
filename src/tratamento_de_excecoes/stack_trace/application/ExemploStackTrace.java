fbc35feef58b68dfabb2efe411e552467114cb98
Viniciusmgm <vinicius.mirandagm@gmail.com>
2021-11-16 14:21:11 -0300

    stack trace

--- src/tratamento_de_excecoes/stack_trace/application/ExemploStackTrace.java
+++ src/tratamento_de_excecoes/stack_trace/application/ExemploStackTrace.java
@@ -1,10 +1,22 @@
-package tratamento_de_excecoes.try_catch.application;
+package tratamento_de_excecoes.stack_trace.application;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class ExemploTryCatch {
+public class ExemploStackTrace {
 	public static void main(String[] args) {
+		method1();
+		System.out.println("End of program");
+	}
+
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
 		Scanner sc = new Scanner(System.in);
 		try {
 			String[] vect = sc.nextLine().split(" ");
@@ -12,10 +24,12 @@ public class ExemploTryCatch {
 			System.out.println(vect[position]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
 		} catch (InputMismatchException e) {
 			System.out.println("Input error");
 		}
-		System.out.println("End of program");
 		sc.close();
+		System.out.println("***METHOD2 END***");
 	}
 }
